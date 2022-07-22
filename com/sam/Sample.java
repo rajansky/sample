e4898146e02872c1d3140db80d2b7322ec2b6bc1
rajansky <rajansky511@gmail.com>
2022-07-22 07:28:20 +0530

    new code added

---
+++ com/sam/Sample.java
@@ -0,0 +1,24 @@
+package com.sam;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Sample {
+	public static void main(String[]args) throws IOException {
+		File f = new File("D:\\raja 10am\\pending topic\\man\\son\\file operation.txt\\time.txt");
+		boolean b =f.createNewFile();
+		System.out.println(b);
+		
+		boolean c = f.canRead();
+		System.out.println(c);
+		
+		boolean d =f.canWrite();
+		System.out.println(d);
+		
+		boolean e = f.isFile();
+		System.out.println(e);
+		
+		boolean h = f.isDirectory();
+		System.out.println(h);
+
+}}
